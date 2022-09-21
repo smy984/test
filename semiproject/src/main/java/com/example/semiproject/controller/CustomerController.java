7b694a392b366be82365b31790203d58ab93e16c
smy984 <smy984@naver.com>
2022-09-22 00:11:39 +0900

    고객 로그인, 회원가입 구현 중

--- semiproject/src/main/java/com/example/semiproject/controller/CustomerController.java
+++ semiproject/src/main/java/com/example/semiproject/controller/CustomerController.java
@@ -3,9 +3,9 @@ package com.example.semiproject.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.semiproject.entity.CustomerDto;
 import com.example.semiproject.repository.CustomerDao;
@@ -23,7 +23,7 @@ public class CustomerController {
 	}
 	
 	@PostMapping("/join")
-	public String join(@RequestParam CustomerDto customerDto) {
+	public String join(@ModelAttribute CustomerDto customerDto) {
 		customerDao.insert(customerDto);
 		return "redirect:login";
 	}
